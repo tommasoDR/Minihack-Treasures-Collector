65668a90b0e34b499f1545311809e8c19e22af2f
root <root@PC-Tommaso.localdomain>
2023-12-03 13:32:39 +0100

    Random room generator

---
+++ src/data.py
@@ -0,0 +1,9 @@
+##### FILE PATH ####
+object_file_path = "src/objects_info.json"
+room_pattern_path = "rooms_pattern/room{}.des"
+
+
+##### CONFIG VARIABLES #####
+num_rooms = 3
+num_patterns = 2
+num_generations_spins = 2
